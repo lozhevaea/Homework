6229dace1aabc10b3ac9035c42f5ce1831c81dee
Elena Kuznetsova <lozhevaea@gmail.com>
2023-04-29 12:01:09 +0500

    add task6

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,6 +3,7 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10 
 // a = -9 b = -3 -> max = -3
+
 Console.Write("Введите первое число: ");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
