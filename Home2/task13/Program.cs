0b0b96bac56a1fff3249a88cc44c3f581fd74371
Elena Kuznetsova <lozhevaea@gmail.com>
2023-05-03 13:57:23 +0500

    add Convert ToDouble

--- Home2/task13/Program.cs
+++ Home2/task13/Program.cs
@@ -5,47 +5,54 @@
 // 78 -> третьей цифры нет
 // 32679345 -> 6
 
-System.Console.WriteLine("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int a = n.ToString().Length;
+// System.Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int a = n.ToString().Length;
 
-int result = 0;
-int b = 1;
+// int result = 0;
+// int b = 1;
 
 // Решение задачи с помощью ветвления и цикла while
-
-if (a < 3)
-{
-    Console.Write("Третьей цифры нет. ");
-}
-else
-{
-int i = a;
-while (i > 3)
-{
-     b = b * 10;
-     i = i - 1;
-}
-result = n / b % 10;
-System.Console.WriteLine(result);
-}
-
-
-//Решение задачи с помощью цикла for
 
 // if (a < 3)
 // {
 //     Console.Write("Третьей цифры нет. ");
 // }
-// else 
+// else
 // {
-// for (int i = a; i > 3; i--)
+// int i = a;
+// while (i > 3)
 // {
-//     b = b * 10;
+//      b = b * 10;
+//      i = i - 1;
 // }
 // result = n / b % 10;
 // System.Console.WriteLine(result);
 // }
+
+
+//Решение задачи с помощью цикла for и с преобразованием вещественных чисел в целые.
+//Если четвертая цифра >5, то идет округление. Не знаю как этого избежать!
+
+System.Console.WriteLine("Введите число: ");
+double n = Convert.ToDouble(Console.ReadLine());
+int a = n.ToString().Length;
+int result = 0;
+int b = 1;
+
+if (a < 3)
+{
+    Console.Write("Третьей цифры нет. ");
+}
+else 
+{
+for (int i = 0; i < a; i++)
+{
+    b = b * 10;
+}
+result = Convert.ToInt32(Convert.ToDouble(n / b * 1000))% 10;
+System.Console.WriteLine(result);
+}
 
 
 // Решение задачи через строковую переменную
