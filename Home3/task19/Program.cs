5e7fec802460250cc33aff9e1cecfec88149ec9d
Elena Kuznetsova <lozhevaea@gmail.com>
2023-05-26 15:29:20 +0500

    add new solution

--- Home3/task19/Program.cs
+++ Home3/task19/Program.cs
@@ -7,13 +7,54 @@
 
 System.Console.WriteLine("Введите пятизначное число: ");
 string? N = Console.ReadLine();
-int n = Convert.ToInt32(Convert.ToString(N));
 
-if (n < 10000 || n > 99999)
+
+
+// Решение с преобразованием из строковой переменной в целое число.
+// int n = Convert.ToInt32(Convert.ToString(N));
+
+// if (n < 10000 || n > 99999)
+// {
+//     System.Console.WriteLine("Число не пятизначное!");
+// }
+// else if (N![0] == N[4] && N[1] == N[3])
+// {
+//     System.Console.WriteLine("Число является палиндромом");
+// }
+// else
+// {
+//     System.Console.WriteLine("Число не является палиндромом");
+// }
+
+
+
+//Простое решение через строковую переменную.
+// if (N!.Length == 5)
+// {
+//     if (N![0] == N[4] && N[1] == N[3])
+//     {
+//         System.Console.WriteLine("Число является палиндромом");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine("Число не является палиндромом");
+//     }
+// }
+// else
+// {
+//     System.Console.WriteLine("Число не пятизначное!");
+// }
+
+
+
+// Проверка числа на пятизначность
+if (N!.Length != 5)
 {
     System.Console.WriteLine("Число не пятизначное!");
+    return;
 }
-else if (N![0] == N[4] && N[1] == N[3])
+
+if (N![0] == N[4] && N[1] == N[3])
 {
     System.Console.WriteLine("Число является палиндромом");
 }
