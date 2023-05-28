faad02e504794690c91e938627e9d4ab0b2a9144
Elena Kuznetsova <lozhevaea@gmail.com>
2023-05-28 11:32:43 +0500

    add new solution task21

--- Home3/task21/Program.cs
+++ Home3/task21/Program.cs
@@ -4,31 +4,47 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-int ReadInt(string text)
-{
-    System.Console.Write(text);
-    return Convert.ToInt32(Console.ReadLine());
-}
+// int ReadInt(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
 
-string[] array = {"Введите координату X точки A: ", 
-                  "Введите координату Y точки A: ", 
-                  "Введите координату Z точки A: ", 
-                  "Введите координату X точки B: ", 
-                  "Введите координату Y точки B: ", 
-                  "Введите координату Z точки B: "};
+// string[] array = {"Введите координату X точки A: ", 
+//                   "Введите координату Y точки A: ", 
+//                   "Введите координату Z точки A: ", 
+//                   "Введите координату X точки B: ", 
+//                   "Введите координату Y точки B: ", 
+//                   "Введите координату Z точки B: "};
 
-int[] coordsA = new int [3];
-coordsA[0] = ReadInt(array[0]);
-coordsA[1] = ReadInt(array[1]);
-coordsA[2] = ReadInt(array[2]);
+// int[] coordsA = new int [3];
+// coordsA[0] = ReadInt(array[0]);
+// coordsA[1] = ReadInt(array[1]);
+// coordsA[2] = ReadInt(array[2]);
 
-int[] coordsB = new int [3];
-coordsB[0] = ReadInt(array[3]);
-coordsB[1] = ReadInt(array[4]);
-coordsB[2] = ReadInt(array[5]);
+// int[] coordsB = new int [3];
+// coordsB[0] = ReadInt(array[3]);
+// coordsB[1] = ReadInt(array[4]);
+// coordsB[2] = ReadInt(array[5]);
 
-double distance = Math.Sqrt(Math.Pow(coordsA[0] - coordsB[0], 2) 
-                          + Math.Pow(coordsA[1] - coordsB[1], 2) 
-                          + Math.Pow(coordsA[2] - coordsB[2], 2));
+// double distance = Math.Sqrt(Math.Pow(coordsA[0] - coordsB[0], 2) 
+//                           + Math.Pow(coordsA[1] - coordsB[1], 2) 
+//                           + Math.Pow(coordsA[2] - coordsB[2], 2));
                           
-System.Console.WriteLine(Math.Round(distance, 2));
+// System.Console.WriteLine(Math.Round(distance, 2));
+
+
+
+// Возможность вводить координаты одной строкой через 
+// пробел и выводить их на экран.
+
+System.Console.WriteLine("Введите координаты в соответветствующем порядке: X, Y, Z");
+int[] coordsA = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+System.Console.WriteLine("Координаты: (Вывод при помощи string.Join)");
+System.Console.WriteLine("[" + string.Join(", ", coordsA) + "]");
+
+System.Console.WriteLine("Координаты: (Вывод при помощи цикла for)");
+for (int i = 0; i < coordsA.Length; i++)
+{
+    System.Console.Write(coordsA[i] + " ");
+}
