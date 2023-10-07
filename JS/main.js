efea7d6d003aa3775d880af1a8cc32e0c6ddc055
Kuznetsova Elena <lozhevaea@gmail.com>
2023-10-08 02:19:26 +0500

    add corection

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,9 @@
 function greeting(name) {
-    alert(`Здравствуйте, ${name}!`);
+    let name1 = name[0].toUpperCase();
+    for (let i = 1; i < name.length; i++) {
+        name1 += name[i].toLowerCase(); 
+    }
+    alert(`Здравствуйте, ${name1}!`);
 }
 
 const n = prompt('Введите Ваше имя');
